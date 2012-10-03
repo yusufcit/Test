a3d8896e606a8fd8c28f96d1b98a741be88aa859
yusufcit <yusufcit@gmail.com>
2012-10-03 22:16:29 +0100

    Just added new Test controller

---
+++ application/controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+class TestController extends Zend_Controller_Action
+{
+
+	public function init()
+	{
+		/* Initialize action controller here */
+	}
+
+	public function indexAction()
+	{
+		echo "hello world. this is Yusuf";
+		// action body
+	}
+
+
+}
+
